02e05d2d4a10e5f354bdd388d1bbafdf27d41c7b
wangjianquanwjq <610743994@qq.com>
2023-03-01 17:18:27 +0800

    1

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,30 @@
+Component({
+  data: {
+    selected: 0,
+    color: "#7A7E83",
+    selectedColor: "#3cc51f",
+    list: [{
+      pagePath: "/pages/record/index",
+      iconPath: "/image/icon_component.png",
+      selectedIconPath: "/image/icon_component_HL.png",
+      text: "对战记录"
+    }, {
+      pagePath: "/pages/my/index",
+      iconPath: "/image/icon_API.png",
+      selectedIconPath: "/image/icon_API_HL.png",
+      text: "我的"
+    }]
+  },
+  attached() {
+  },
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({url})
+      this.setData({
+        selected: data.index
+      })
+    }
+  }
+})
